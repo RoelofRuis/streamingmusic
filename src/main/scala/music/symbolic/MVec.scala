36c92981d366476a9d19b4f8111b73fd68e35230
RoelofRuis <roelof.ruis@gmail.com>
2017-12-14 20:46:49 +0100

    Fixed intervals spanning octaves breaking the summation

--- src/main/scala/music/symbolic/MVec.scala
+++ src/main/scala/music/symbolic/MVec.scala
@@ -2,10 +2,10 @@ package music.symbolic
 
 case class MVec(n: Int, a: Int = 0) {
   def +(other: MVec)(implicit ns: NotationSystem): MVec = {
-    val newN = (this.n + other.n) % ns.numNotes
+    val newN = this.n + other.n
     val newNSteps = ns.steps(newN)
-    val actualSteps = (ns.steps(this.n) + ns.steps(other.n) + this.a + other.a) % ns.numSteps
+    val actualSteps = ns.steps(this.n) + ns.steps(other.n) + this.a + other.a
 
-    MVec(newN, actualSteps - newNSteps)
+    MVec(newN % ns.numNotes, actualSteps - newNSteps)
   }
 }
--- src/main/scala/music/symbolic/NotationSystem.scala
+++ src/main/scala/music/symbolic/NotationSystem.scala
@@ -6,6 +6,11 @@ case class NotationSystem(s: Seq[Int]) {
   def numSteps: Int = s.sum
 
   def steps(n: Int): Int = {
-    s.slice(0, n).sum
+    @annotation.tailrec
+    def loop(n: Int, acc: Int): Int = {
+      if (n <= numNotes) s.slice(0, n).sum + acc
+      else loop(n - numNotes, s.sum)
+    }
+    loop(n, 0)
   }
 }
