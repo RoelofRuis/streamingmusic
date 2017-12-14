aa2f16480fe62d9fb07753702ffd418c20eecb6a
RoelofRuis <roelof.ruis@gmail.com>
2017-12-14 19:36:56 +0100

    Made chord aggregator use correct note class

--- src/main/scala/graph/ChordAggregator.scala
+++ src/main/scala/graph/ChordAggregator.scala
@@ -2,17 +2,17 @@ package graph
 
 import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
 import akka.stream.{Attributes, FlowShape, Inlet, Outlet}
-import music.{MusicEvent, Note, NoteOff, NoteOn}
+import music.{MusicEvent, MidiNote, NoteOff, NoteOn}
 
-class ChordAggregator extends GraphStage[FlowShape[MusicEvent, Set[Note]]] {
+class ChordAggregator extends GraphStage[FlowShape[MusicEvent, Set[MidiNote]]] {
   val in: Inlet[MusicEvent] = Inlet[MusicEvent]("graph.ChordAgregator.in")
-  val out: Outlet[Set[Note]] = Outlet[Set[Note]]("graph.ChordAgregator.out")
+  val out: Outlet[Set[MidiNote]] = Outlet[Set[MidiNote]]("graph.ChordAgregator.out")
 
-  val shape: FlowShape[MusicEvent, Set[Note]] = FlowShape.of(in, out)
+  val shape: FlowShape[MusicEvent, Set[MidiNote]] = FlowShape.of(in, out)
 
   override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
     new GraphStageLogic(shape) {
-      var activeNotes: Set[Note] = Set()
+      var activeNotes: Set[MidiNote] = Set()
 
       setHandler(in, new InHandler {
         override def onPush(): Unit = {
