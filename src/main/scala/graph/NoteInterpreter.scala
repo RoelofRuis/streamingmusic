e85528a62b4d69baa8020f99fbe358e1dfb73e8c
RoelofRuis <roelof.ruis@gmail.com>
2017-12-27 18:47:48 +0100

    Interpreting a pitch now returns a set of vectors

--- src/main/scala/graph/NoteInterpreter.scala
+++ src/main/scala/graph/NoteInterpreter.scala
@@ -6,11 +6,11 @@ import akka.stream.{Attributes, FlowShape, Inlet, Outlet}
 import music.MidiNote
 import music.symbolic.{MVec, NotationSystem, NoteName, StandardNotation}
 
-class NoteInterpreter extends GraphStage[FlowShape[Set[MidiNote], Set[List[String]]]] {
+class NoteInterpreter extends GraphStage[FlowShape[Set[MidiNote], Set[String]]] {
   val in: Inlet[Set[MidiNote]] = Inlet[Set[MidiNote]]("graph.NoteInterpreter.in")
-  val out: Outlet[Set[List[String]]] = Outlet[Set[List[String]]]("graph.NoteInterpreter.out")
+  val out: Outlet[Set[String]] = Outlet[Set[String]]("graph.NoteInterpreter.out")
 
-  val shape: FlowShape[Set[MidiNote], Set[List[String]]] = FlowShape.of(in, out)
+  val shape: FlowShape[Set[MidiNote], Set[String]] = FlowShape.of(in, out)
 
   override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
     new GraphStageLogic(shape) with StageLogging {
@@ -19,7 +19,7 @@ class NoteInterpreter extends GraphStage[FlowShape[Set[MidiNote], Set[List[Strin
       setHandler(in, new InHandler {
         override def onPush(): Unit = {
           val midiNotes = grab[Set[MidiNote]](in)
-          val noteList = midiNotes.map(ns.midi2pc).map(MVec(0, 0).interpretRelative(_).map(NoteName(_)))
+          val noteList = midiNotes.map(ns.midi2pc).flatMap(MVec(0, 0).interpretRelative(_).map(NoteName(_)))
 
           push(out, noteList)
         }
