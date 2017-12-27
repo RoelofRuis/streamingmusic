e648a5408c1a9a3f663da26ece94b8b4fb35c0b0
RoelofRuis <roelof.ruis@gmail.com>
2017-12-27 18:18:37 +0100

    added some command line args parsing to allow reading from serial or file

--- src/main/scala/StreamMidi.scala
+++ src/main/scala/StreamMidi.scala
@@ -1,43 +1,61 @@
 import java.nio.file.Paths
 
 import akka.actor.ActorSystem
-import akka.event.Logging
 import akka.serial.stream.Serial
-import akka.serial.stream.Serial.Connection
 import akka.serial.{Parity, SerialSettings}
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{FileIO, Flow, Sink, Source}
+import akka.stream.scaladsl.{FileIO, Sink, Source}
 import akka.util.ByteString
 import graph.{BytestringSplitter, ChordAggregator, MusicEventParser, NoteInterpreter}
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 
 object StreamMidi extends App {
 
+  val argsList: Map[String, Any] = ArgParser.parseOptions(Map(), args.toList)
+
   implicit val system: ActorSystem = ActorSystem("midiserial")
-  val log = Logging(system.eventStream, "my.nice.string")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val executionContext: ExecutionContext = system.dispatcher
 
+  val source = {
+    if (argsList.contains("file")) {
+      println(s"Reading from file ${argsList("file")}")
+      openFileSource(argsList("file").toString)
+    } else {
+      println("Trying to open /dev/ttyAMA0")
+      openSerialSource
+    }
+  }
 
-  val settings: SerialSettings = SerialSettings(
-    baud = 38400,
-    characterSize = 8,
-    twoStopBits = false,
-    parity = Parity.None
-  )
-
-  println("Trying to open /dev/ttyAMA0")
-  val serial: Flow[ByteString, ByteString, Future[Connection]] = Serial().open("/dev/ttyAMA0", settings)
-
-  val fileSource: Source[ByteString, _] = FileIO.fromPath(Paths.get("raw/sample_cmajorscale.dat"))
-
-  Source.empty
-    .via(serial)
+  source
     .via(new BytestringSplitter())
     .via(new MusicEventParser())
     .via(new ChordAggregator())
     .via(new NoteInterpreter())
     .runWith(Sink.foreach(println))
 
+  def openFileSource(path: String): Source[ByteString, _] = FileIO.fromPath(Paths.get(path))
+
+  def openSerialSource: Source[ByteString, _] = {
+    val settings: SerialSettings = SerialSettings(
+      baud = 38400,
+      characterSize = 8,
+      twoStopBits = false,
+      parity = Parity.None
+    )
+
+    Source.empty.via(Serial().open("/dev/ttyAMA0", settings))
+  }
+}
+
+object ArgParser {
+  def parseOptions(map: Map[String, Any], list: List[String]): Map[String, Any] = {
+    list match {
+      case Nil => map
+      case "-f" :: value :: tail => parseOptions(map ++ Map("file" -> value), tail)
+      case option :: tail => println("Unknown option "+option)
+        sys.exit(0)
+    }
+  }
 }
