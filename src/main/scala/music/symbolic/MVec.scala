7f4641dcea750280bbc0c8d1199aee5c606d6b00
RoelofRuis <roelof.ruis@gmail.com>
2017-12-30 01:06:37 +0100

    long chain on separate lines

--- src/main/scala/music/symbolic/MVec.scala
+++ src/main/scala/music/symbolic/MVec.scala
@@ -32,7 +32,11 @@ case class MVec(step: NotationSystem#Step, acc: NotationSystem#Accidental = 0) {
     def tryMvec(pc: NotationSystem#PitchClass, mod: Int): Option[MVec] = {
       ns.stepInScale(pc + mod).map(MVec(_, -mod))
     }
-    Range.inclusive(-acc-2, -acc+2).flatMap(i => tryMvec(pc, i)).toSet[MVec].map(_ - this).map(_.rectify)
+    Range.inclusive(-acc-2, -acc+2)
+      .flatMap(i => tryMvec(pc, i))
+      .toSet[MVec]
+      .map(_ - this)
+      .map(_.rectify)
   }
 
   override def toString: String = s"[$step,$acc]"
