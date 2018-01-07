c2a74e21a0c1006ac1325a8cb554d2d4f59ed41f
RoelofRuis <roelof.ruis@gmail.com>
2018-01-07 23:50:00 +0100

    added some type of sorting, not quite sure if it is convenient

--- src/main/scala/music/symbolic/IntervalFunction.scala
+++ src/main/scala/music/symbolic/IntervalFunction.scala
@@ -1,24 +1,27 @@
 package music.symbolic
 
-sealed trait IntervalFunction
+sealed trait IntervalFunction extends Comparable[IntervalFunction] {
+  val value: Int
+  override def compareTo(other: IntervalFunction): Int = value.compareTo(other.value)
+}
 
-case object Root extends IntervalFunction
-case object Two extends IntervalFunction
-case object FlatThree extends IntervalFunction
-case object Three extends IntervalFunction
-case object Four extends IntervalFunction
-case object FlatFive extends IntervalFunction
-case object Five extends IntervalFunction
-case object SharpFive extends IntervalFunction
-case object Six extends IntervalFunction
-case object DiminishedSeven extends IntervalFunction
-case object FlatSeven extends IntervalFunction
-case object Seven extends IntervalFunction
-case object FlatNine extends IntervalFunction
-case object Nine extends IntervalFunction
-case object FlatTen extends IntervalFunction
-case object Eleven extends IntervalFunction
-case object SharpEleven extends IntervalFunction
-case object FlatThirteen extends IntervalFunction
-case object Thirteen extends IntervalFunction
+case object Root extends IntervalFunction {val value = 1}
+case object Two extends IntervalFunction {val value = 2}
+case object FlatThree extends IntervalFunction {val value = 3}
+case object Three extends IntervalFunction {val value = 4}
+case object Four extends IntervalFunction {val value = 5}
+case object FlatFive extends IntervalFunction {val value = 6}
+case object Five extends IntervalFunction {val value = 7}
+case object SharpFive extends IntervalFunction {val value = 8}
+case object Six extends IntervalFunction {val value = 9}
+case object DiminishedSeven extends IntervalFunction {val value = 10}
+case object FlatSeven extends IntervalFunction {val value = 11}
+case object Seven extends IntervalFunction {val value = 12}
+case object FlatNine extends IntervalFunction {val value = 13}
+case object Nine extends IntervalFunction {val value = 14}
+case object FlatTen extends IntervalFunction {val value = 15}
+case object Eleven extends IntervalFunction {val value = 16}
+case object SharpEleven extends IntervalFunction {val value = 17}
+case object FlatThirteen extends IntervalFunction {val value = 18}
+case object Thirteen extends IntervalFunction {val value = 19}
 
