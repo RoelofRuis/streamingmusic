a7bb4e1bd95b83b9a38eef3e993b89be8a2a64e0
RoelofRuis <roelof.ruis@gmail.com>
2018-01-17 02:28:47 +0100

    Added lookup tree data structure

---
+++ src/main/scala/util/LookupTree.scala
@@ -0,0 +1,24 @@
+package util
+
+/**
+  * Data structure that can be used for linear time lookup of B given a list of A.
+  */
+case class LookupTree[A, B](subNodes: Map[A, LookupTree[A, B]], nodeValue: Option[B]) {
+  def store(l: List[A], value: B): LookupTree[A, B] = {
+    if (l.isEmpty) LookupTree(subNodes, Some(value))
+    else LookupTree(subNodes + (l.head -> subNodes.getOrElse(l.head, LookupTree(Map(), None)).store(l.tail, value)), nodeValue)
+  }
+
+  def find(l: List[A]): Option[B] = {
+    if (l.isEmpty) nodeValue
+    else subNodes.get(l.head).flatMap(_.find(l.tail))
+  }
+}
+
+object LookupTree {
+  def empty[A, B]: LookupTree[A, B] = LookupTree[A, B](Map(), None)
+
+  def build[A, B](data: Map[B, List[A]]): LookupTree[A, B] = {
+    data.foldLeft[LookupTree[A, B]](empty){case (tree, (value, list)) => tree.store(list, value)}
+  }
+}
