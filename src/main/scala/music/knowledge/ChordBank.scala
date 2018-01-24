6dff7a2a4aac9d6e07bfe344d15b7f7e319e5335
RoelofRuis <roelof.ruis@gmail.com>
2018-01-24 18:50:42 +0100

    different names and more definitions

--- src/main/scala/music/knowledge/ChordBank.scala
+++ src/main/scala/music/knowledge/ChordBank.scala
@@ -12,35 +12,50 @@ import util.{LookupTree, Requirements}
 object ChordBank {
   private val chordData: Map[List[IntervalFunction], ChordQuality] = Requirements.expand(Map(
     // 3 note chords
-    List(always(root), always(three), always(five)) -> ChordQuality("Major"),
-    List(always(root), always(flatThree), always(five)) -> ChordQuality("Minor"),
-    List(always(root), always(flatThree), always(flatFive)) -> ChordQuality("Diminished") ,
-    List(always(root), always(three), always(sharpFive)) -> ChordQuality("Augmented"),
-    List(always(root), always(two), always(five)) -> ChordQuality("Sus 2"),
-    List(always(root), always(four), always(five)) -> ChordQuality("Sus 4"),
+    List(always(root), always(three), always(five)) -> ChordQuality(""),
+    List(always(root), always(flatThree), always(five)) -> ChordQuality("m"),
+    List(always(root), always(flatThree), always(flatFive)) -> ChordQuality("dim") ,
+    List(always(root), always(three), always(sharpFive)) -> ChordQuality("#5"),
+    List(always(root), always(two), always(five)) -> ChordQuality("sus 2"),
+    List(always(root), always(four), always(five)) -> ChordQuality("sus 4"),
 
     // 4< note chords
-    List(always(root), always(three), always(five), always(six)) -> ChordQuality("Major 6"),
-    List(always(root), always(flatThree), always(five), always(six)) -> ChordQuality("Minor 6"),
-    List(always(root), always(three), maybe(five), always(seven)) -> ChordQuality("Major 7"),
-    List(always(root), always(flatThree), maybe(five), always(seven)) -> ChordQuality("Minor M7"),
-    List(always(root), always(flatThree), maybe(five), always(flatSeven)) -> ChordQuality("Minor 7"),
-    List(always(root), always(three), maybe(five), always(flatSeven)) -> ChordQuality("Dominant 7"),
-    List(always(root), always(flatThree), always(flatFive), always(flatSeven)) -> ChordQuality("Half Diminished 7"),
-    List(always(root), always(flatThree), always(flatFive), always(diminishedSeven)) -> ChordQuality("Diminished 7"),
-    List(always(root), always(three), always(five), always(nine)) -> ChordQuality("Major add 9"),
+    List(always(root), always(three), always(five), always(six)) -> ChordQuality("6"),
+    List(always(root), always(flatThree), always(five), always(six)) -> ChordQuality("m6"),
+    List(always(root), always(three), maybe(five), always(seven)) -> ChordQuality("Δ7"),
+    List(always(root), always(flatThree), maybe(five), always(seven)) -> ChordQuality("m Δ7"),
+    List(always(root), always(flatThree), maybe(five), always(flatSeven)) -> ChordQuality("m7"),
+    List(always(root), always(three), maybe(five), always(flatSeven)) -> ChordQuality("7"),
+    List(always(root), always(flatThree), always(flatFive), always(flatSeven)) -> ChordQuality("ø"),
+    List(always(root), always(flatThree), always(flatFive), always(diminishedSeven)) -> ChordQuality("o"),
+    List(always(root), always(three), always(five), always(nine)) -> ChordQuality("Δ/add9"),
+    List(always(root), always(three), always(sharpFive), always(seven)) -> ChordQuality("#5/7")
 
     // 5< note chords
-    List(always(root), always(three), always(five), always(six), always(nine)) -> ChordQuality("Major 6 9"),
-    List(always(root), always(three), maybe(five), always(flatSeven), always(nine)) -> ChordQuality("Dominant 9"),
-    List(always(root), always(three), maybe(five), always(flatSeven), always(flatNine)) -> ChordQuality("Dominant b9"),
-    List(always(root), always(three), maybe(five), always(seven), always(nine)) -> ChordQuality("Major 9"),
-    List(always(root), always(three), maybe(five), always(flatSeven), always(flatTen)) -> ChordQuality("Dominant b10"),
+    List(always(root), always(three), always(five), always(six), always(nine)) -> ChordQuality("6/9"),
+    List(always(root), always(three), maybe(five), always(flatSeven), always(nine)) -> ChordQuality("9"),
+    List(always(root), always(three), maybe(five), always(seven), always(nine)) -> ChordQuality("Δ9"),
+    List(always(root), always(flatThree), maybe(five), always(flatSeven), always(nine)) -> ChordQuality("m9"),
+    List(always(root), always(three), maybe(five), always(flatSeven), always(flatNine)) -> ChordQuality("7/b9"),
+    List(always(root), always(three), maybe(five), always(flatSeven), always(flatTen)) -> ChordQuality("7/b10"),
+    List(always(root), always(seven), always(nine), always(eleven)) -> ChordQuality("Δ11 (sus)"),
 
     // 6< note chords
-    List(always(root), always(three), maybe(five), always(seven), maybe(nine), always(eleven)) -> ChordQuality("Major 11"),
-    List(always(root), always(three), maybe(five), always(flatSeven), maybe(nine), always(eleven)) -> ChordQuality("Dominant 11"),
-    List(always(root), always(three), maybe(five), always(flatSeven), maybe(nine), always(sharpEleven)) -> ChordQuality("Dominant #11"),
+    List(always(root), always(three), maybe(five), always(flatSeven), maybe(nine), always(eleven)) -> ChordQuality("11"),
+    List(always(root), always(three), maybe(five), always(seven), maybe(nine), always(eleven)) -> ChordQuality("Δ11"),
+    List(always(root), always(flatThree), maybe(five), always(flatSeven), maybe(nine), always(eleven)) -> ChordQuality("m11"),
+    List(always(root), always(three), maybe(five), always(flatSeven), maybe(nine), always(sharpEleven)) -> ChordQuality("7/#11"),
+    List(always(root), always(three), maybe(five), always(flatSeven), always(flatNine), always(sharpEleven)) -> ChordQuality("7/b9/#11"),
+    List(always(root), always(three), maybe(five), always(flatSeven), always(flatTen), always(flatThirteen)) -> ChordQuality("7/b10/b13"),
+
+    // 7< note chords
+    List(always(root), always(three), maybe(five), always(flatSeven), maybe(nine), maybe(eleven), always(thirteen)) -> ChordQuality("13"),
+    List(always(root), always(three), maybe(five), always(seven), maybe(nine), maybe(eleven), always(thirteen)) -> ChordQuality("Δ13"),
+    List(always(root), always(flatThree), maybe(five), always(flatSeven), maybe(nine), maybe(eleven), always(thirteen)) -> ChordQuality("m13"),
+    List(always(root), always(three), maybe(five), always(flatSeven), always(flatNine), maybe(eleven), always(thirteen)) -> ChordQuality("7/b9/13"),
+    List(always(root), always(three), maybe(five), always(flatSeven), always(flatNine), maybe(eleven), always(flatThirteen)) -> ChordQuality("7/b9/b13"),
+    List(always(root), always(three), always(flatFive), always(flatSeven), always(flatNine), maybe(eleven), always(flatThirteen)) -> ChordQuality("7/b5/b9/b13 (alt)"),
+    List(always(root), always(three), maybe(five), always(flatSeven), maybe(nine), always(sharpEleven), always(thirteen)) -> ChordQuality("7/#11/13"),
   ))
 
   private val chordTree = LookupTree.build[IntervalFunction, ChordQuality](chordData)
