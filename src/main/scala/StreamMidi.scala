e7f2ee9d2c71c5940f963c4c7ce0943d646a4db1
RoelofRuis <roelof.ruis@gmail.com>
2018-12-09 21:56:00 +0100

    Configuring simple midi IO

--- src/main/scala/StreamMidi.scala
+++ src/main/scala/StreamMidi.scala
@@ -2,12 +2,12 @@ import java.nio.file.Paths
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{FileIO, Sink}
+import akka.stream.scaladsl.{FileIO, Keep, Sink}
+import akka.util.ByteString
 import com.typesafe.config.{Config, ConfigFactory}
-import io.Sources
-import stream.MessageParser
+import io.Midi
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 object StreamMidi extends App {
 
@@ -17,10 +17,20 @@ object StreamMidi extends App {
 
   val config: Config = ConfigFactory.load("application.conf")
 
-  Sources.midiSerial("/dev/ttyAMA0")
-    .alsoTo(FileIO.toPath(Paths.get("test.bin")))
-    .mapConcat(_.toVector)
-    .via(new MessageParser)
-    .to(Sink.foreach(println))
+  val source = Midi.serialSource(config.getString("midi.in"))
+  val sink   = makeSink(config.getConfig("midi.out"))
 
+  val (serialIn, serialOut) = source.toMat(sink)(Keep.both).run()
+
+  serialIn.onComplete(s => println(s"Serial In:$s\n"))
+  serialOut.onComplete(s => println(s"Serial Out: $s\n"))
+
+  def makeSink(conf: Config): Sink[ByteString, Future[Any]] = {
+    val path = conf.getString("path")
+    conf.getString("resource") match {
+      case "file"   => FileIO.toPath(Paths.get(path))
+      case "serial" => Midi.serialSink(path)
+      case r => throw new RuntimeException(s"unknown resource option $r")
+    }
+  }
 }
---
+++ src/main/scala/io/Midi.scala
@@ -0,0 +1,28 @@
+package io
+
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.serial.stream.Serial
+import akka.serial.{Parity, SerialSettings}
+import akka.stream.scaladsl.{Keep, Sink, Source}
+import akka.util.ByteString
+
+import scala.concurrent.Future
+
+object Midi {
+  private val SerialMidiSettings = SerialSettings(
+    baud = 38400,
+    characterSize = 8,
+    twoStopBits = false,
+    parity = Parity.None
+  )
+
+  def serialSource(port: String)(implicit actorSystem: ActorSystem): Source[ByteString, Future[Serial.Connection]] = {
+    Source.empty.viaMat(Serial().open(port, SerialMidiSettings))(Keep.right[NotUsed, Future[Serial.Connection]])
+  }
+
+  def serialSink(port: String)(implicit actorSystem: ActorSystem): Sink[ByteString, Future[Serial.Connection]] = {
+    Serial().open(port, SerialMidiSettings)
+      .to(Sink.ignore)
+  }
+}
